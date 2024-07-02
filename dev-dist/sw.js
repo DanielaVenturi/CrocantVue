c25b9af14e384cbbb94fe580c03e8ac01a985b50
DanielaVenturi <danielaventuri14@gmail.com>
2024-07-02 11:11:03 -0300

    Criando um componente para o logo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ujsj5numdpg"
+    "revision": "0.4veormub18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
