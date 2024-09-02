df1a9cdbe1f86c8ad649944ced9175099ff76653
DanielaVenturi <danielaventuri14@gmail.com>
2024-09-02 15:01:07 -0300

    css da navbar nas telas médias

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e29hb12s42g"
+    "revision": "0.ck3pi6g1f48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
