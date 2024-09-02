edcc7555aee8c58580073d16fdd3fe8c292330a3
DanielaVenturi <danielaventuri14@gmail.com>
2024-09-02 11:51:50 -0300

    mudanças das cores do rodapé

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e29hb12s42g"
+    "revision": "0.6qn3sslbel8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
