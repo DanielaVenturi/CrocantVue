1a72cf2be8614e92eb7c0219f477f7911d2c3548
brunaanjos <brunaanjosdaveiga@gmail.com>
2024-10-01 11:10:39 -0300

    Ajustes finos do CSS da tela large #1, #14

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,11 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
+<<<<<<< Updated upstream
     "revision": "0.lp9ronrbuog"
+=======
+    "revision": "0.q6mhjth3648"
+>>>>>>> Stashed changes
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
